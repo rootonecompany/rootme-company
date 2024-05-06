1dc567f734cda3d15ee4d341fff42243be83e061
mjkkca1b <mjkkca1b@naver.com>
2024-05-06 15:51:27 +0900

    add: css,js 수정사항

--- js/rootme-company-main.js
+++ js/rootme-company-main.js
@@ -11,6 +11,26 @@ gsap.ticker.add((time) => {
 
 gsap.ticker.lagSmoothing(0);
 
+// 비디오 늘리기
+const main_video = gsap.timeline();
+
+main_video.to(".main_video_object", {
+  scale: 1,
+  width: "100vw",
+  height: "100vh",
+});
+
+ScrollTrigger.create({
+  animation: main_video,
+  trigger: ".main_video",
+  start: "top top",
+  end: "bottom top",
+  scrub: true,
+  pin: true,
+  markers: false,
+  anticipatePin: 1,
+});
+
 
 // about
 gsap
@@ -47,3 +67,27 @@ function showListBox03() {
   gsap.to(".about_txt3", { opacity: 1, y: 0 });
 }
 
+
+//header_gnb
+document.addEventListener("DOMContentLoaded", function () {
+  const header = document.querySelector('.header_gnb');
+  const mainVideo = document.querySelector('.main_video');
+  let lastScrollTop = 0;
+  const triggerOffset = 570;
+
+  window.addEventListener("scroll", function () {
+    let st = window.pageYOffset || document.documentElement.scrollTop;
+
+    if (st > lastScrollTop && st > mainVideo.offsetTop + triggerOffset) {
+
+      if (!header.classList.contains('hidden')) {
+        header.classList.add('hidden');
+      }
+    } else {
+      if (header.classList.contains('hidden')) {
+        header.classList.remove('hidden');
+      }
+    }
+    lastScrollTop = st <= 0 ? 0 : st;
+  }, false);
+});
