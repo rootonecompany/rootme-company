01d815568cfde3344f5d6f34506e7ce19d04c960
dodam24 <dodam0724@gmail.com>
2024-05-09 18:02:17 +0900

    card animation 수정 중

--- js/animation.js
+++ js/animation.js
@@ -1,3 +1,4 @@
+// drop up
 document.addEventListener("DOMContentLoaded", function () {
     var famsiteBtn = document.getElementById("famsite_btn");
     var famsiteMenu = document.querySelector(".famsite_menubar");
@@ -17,3 +18,41 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     });
 });
+
+// box 애니메이션
+// let lastScrollTop = 0;
+
+// window.addEventListener(
+//     "scroll",
+//     function () {
+//         let currentScroll = window.pageYOffset || document.documentElement.scrollTop;
+//         if (currentScroll > lastScrollTop) {
+//             document.querySelector(".box_wrap").classList.remove("reverse");
+//         } else {
+//             document.querySelector(".box_wrap").classList.add("reverse");
+//         }
+//         lastScrollTop = currentScroll <= 0 ? 0 : currentScroll;
+//     },
+//     false
+// );
+
+// box 애니메이션 수정 중
+window.addEventListener('scroll', () => {
+  sliding();
+});
+const slideContainer = document.querySelector('.story')// 전체 또는 story (?)
+const stickyBox = document.querySelector('.story1');
+const boxWrap = document.querySelector('.box_wrap');
+const boxs = document.querySelectorAll('.box')
+const boxLength = boxs.length;
+
+const containerHeight = slideContainer.offsetHeight - stickyBox.offsetHeight;
+
+function sliding() {
+  const ratio = scrollY / (containerHeight / 2);
+  if (ratio < 1) {
+    boxWrap.style.transform = `translateX(${100 - ratio * 100}vw)`;
+  } else {
+    boxWrap.style.transform = `translateX(0vw)`;
+  }
+}
