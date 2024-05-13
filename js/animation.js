8674d002db43f3d42f9fc53a5b08547f01207392
dodam24 <dodam0724@gmail.com>
2024-05-13 11:06:45 +0900

    update: layout 수정

--- js/animation.js
+++ js/animation.js
@@ -1,52 +1,56 @@
 // 카드 스크롤 애니메이션
 class CardFlipOnScroll {
-    constructor(wrapper, story) {
-        this.wrapper = wrapper;
-        this.story = story;
-        this.cards = story.querySelectorAll(".story_card");
+    constructor(storyCardWrap, sticky) {
+        this.storyCardWrap = storyCardWrap;
+        this.sticky = sticky;
+        this.cards = storyCardWrap.querySelectorAll(".story_card");
         this.length = this.cards.length;
 
         this.start = 0;
         this.end = 0;
         this.step = 0;
     }
+
     init() {
-        this.start = this.wrapper.offsetTop;
-        this.end = this.wrapper.offsetTop + this.wrapper.offsetHeight - innerHeight;
+        this.start = this.storyCardWrap.offsetTop;
+        this.end =
+            this.storyCardWrap.offsetTop + this.storyCardWrap.offsetHeight - innerHeight;
         this.step = (this.end - this.start) / (this.length * 2);
     }
+
     animate() {
         this.cards.forEach((card, i) => {
             const s = this.start + this.step * i;
-            const e = x + this.step * (this.length + 1);
+            const e = s + this.step * (this.length + 1);
 
             if (scrollY <= s) {
                 card.style.transform = `
-          perspective(100vw)
-          translateX(100vw)
-        `;
+                  perspective(100vw)
+                  translateX(100vw)
+              `;
             } else if (scrollY > s && scrollY <= e - this.step) {
                 card.style.transform = `
-          perspective(100vw)
-          translateX(${100 + ((scrollY - s) / (e - s)) * -100}vw)
-        `;
+                  perspective(100vw)
+                  translateX(${100 + ((scrollY - s) / (e - s)) * -100}vw)
+              `;
             } else if (scrollY > e - this.step && scrollY <= e) {
                 card.style.transform = `
-          perspective(100vw)
-          translateX(${100 + ((scrollY - s) / (e - s)) * -100}vw)
-        `;
+                  perspective(100vw)
+                  translateX(${100 + ((scrollY - s) / (e - s)) * -100}vw)
+              `;
             } else if (scrollY > e) {
                 card.style.transform = `
-          perspective(100vw)
-          translateX(0vw)
-        `;
+                perspective(100vw)
+                translateX(0vw)
+              `;
             }
         });
     }
 }
-const story = document.querySelector(".story");
+
 const storyCardWrap = document.querySelector(".story_card_wrap");
-const cardFlipOnScroll = new CardFlipOnScroll(story, storyCardWrap);
+const sticky = document.querySelector(".sticky");
+const cardFlipOnScroll = new CardFlipOnScroll(storyCardWrap, sticky);
 cardFlipOnScroll.init();
 
 window.addEventListener("scroll", () => {
