01c1e21a3b651de239849d269b17bc6c4ba5f2c2
dodam24 <dodam0724@gmail.com>
2024-05-17 19:53:15 +0900

    update: story 배경 이미지 비율 조정

--- js/animation.js
+++ js/animation.js
@@ -1,25 +1,22 @@
 // gsap 스크롤 트리거
-const sections = gsap.utils.toArray(".story_card");
+// const sections = gsap.utils.toArray(".story_card");
 
-gsap.to(sections, {
-    xPercent: -65 * sections.length,
-    ease: "none",
-    scrollTrigger: {
-        trigger: ".story_card_wrap",
-        pin: true,
-        start: "center center",
-        end: "bottom top",
-        pinSpacing: true,
-        scrub: 2,
-        snap: 1 / (sections.length - 1),
-        onComplete: function () {
-            setTimeout(function () {
-                gsap.to(window, { scrollTo: { y: "+=100" }, duration: 1 });
-            }, 10000);
-        },
-    },
-    x: 500,
-});
+// gsap.to(sections, {
+//     xPercent: -65 * (sections.length - 1),
+//     ease: "none",
+//     scrollTrigger: {
+//         trigger: ".story_card_wrap",
+//         pin: true,
+//         start: "center center",
+//         // end: "bottom top",
+//         end: () => "+=" + document.querySelector(".story_card_wrap").offsetHeight,
+//         pinSpacing: true,
+//         scrub: 3,
+//         snap: 1 / (sections.length - 1),
+//     },
+//     // x: -100,
+//     duration: 5,
+// });
 
 // 드롭다운 메뉴
 function dropdownFunction() {
