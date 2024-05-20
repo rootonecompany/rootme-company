27c39158ad84f3b619d672f8a963bfadab8647db
dodam24 <dodam0724@gmail.com>
2024-05-20 10:37:59 +0900

    update: 카드 애니메이션 수정

--- js/animation.js
+++ js/animation.js
@@ -1,21 +1,83 @@
 // gsap 스크롤 트리거
-const panels = gsap.utils.toArray(".story_card");
+// const panels = gsap.utils.toArray(".story_card");
 
-gsap.to(panels, {
-    xPercent: -65 * (panels.length - 1),
-    ease: "none",
-    scrollTrigger: {
-        trigger: ".story_card_wrap",
-        pin: ".story_card_wrap",
-        start: "center center",
-        end: "bottom bottom",
-        pinSpacing: true,
-        scrub: 2,
-        snap: 1 / (panels.length - 1),
-        toggleActions: "restart pause resume pause",
-    },
-    x: 100,
-    duration: 5,
+// let mm = gsap.matchMedia();
+
+// mm.add("(min-width: 769px)", () => {
+//     gsap.to(panels, {
+//         xPercent: -65 * (panels.length - 1),
+//         ease: "none",
+//         scrollTrigger: {
+//             trigger: ".story_card_wrap",
+//             pin: ".story_card_wrap",
+//             start: "center center",
+//             end: "bottom bottom",
+//             pinSpacing: true,
+//             scrub: 3,
+//             snap: 1 / (panels.length - 1),
+//             snap: {
+//                 snapTo: 1 / (panels.length - 1),
+//                 delay: 3,
+//             },
+//             // onEnter onLeave onEnterBack onLeaveBack
+//             toggleActions: "restart pause resume pause",
+//         },
+//         x: 100,
+//         duration: 5,
+//     });
+// });
+
+// 카드 스크롤 test
+class CardFlipOnScroll {
+    constructor(wrapper, sticky) {
+        this.wrapper = wrapper;
+        this.sticky = sticky;
+        this.cards = sticky.querySelectorAll(".story_card");
+        this.length = this.cards.length;
+
+        this.start = 0;
+        this.end = 0;
+        this.step = 0;
+    }
+    init() {
+        this.start = this.wrapper.offsetTop;
+        this.end = this.wrapper.offsetTop + this.wrapper.offsetHeight - innerHeight * 1.2;
+        this.step = (this.end - this.start) / this.length;
+    }
+    animate() {
+        this.cards.forEach((card, i) => {
+            const s = this.start + this.step * i;
+            const e = s + this.step * (this.length + 1);
+
+            if (scrollY <= s) {
+                card.style.transform = `
+                    perspective(100vw)
+                    translateX(100vw)
+                `;
+            } else if (scrollY > s && scrollY <= e) {
+                card.style.transform = `
+                    perspective(100vw)
+                    translateX(${100 - ((scrollY - s) / (e - s)) * 150}vw)
+                `;
+            } else if (scrollY > e) {
+                // card.style.transform = `
+                //     perspective(100vw)
+                //     translateX(0vw)
+                // `;
+            }
+        });
+    }
+}
+const mainContent = document.querySelector(".story_card_wrap");
+const sticky = document.querySelector(".story_sticky");
+const cardFlipOnScroll = new CardFlipOnScroll(mainContent, sticky);
+cardFlipOnScroll.init();
+
+window.addEventListener("scroll", () => {
+    cardFlipOnScroll.animate();
+});
+window.addEventListener("resize", () => {
+    cardFlipOnScroll.init();
 });
 
 // 드롭다운 메뉴
