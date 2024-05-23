9579e509c4a39b4e6f29498543f8d0426610e7f3
dodam24 <dodam0724@gmail.com>
2024-05-23 11:01:32 +0900

    update: 반응형 font size 통일

--- js/animation.js
+++ js/animation.js
@@ -3,27 +3,25 @@ const cardWrap = document.querySelector(".story_card_wrap");
 const cardFrame = document.querySelector(".story_card_frame");
 const panels = gsap.utils.toArray(".story_card");
 
-// let mm = gsap.matchMedia();
+let mm = gsap.matchMedia();
 
-// mm.add("(min-width: 769px)", () => {
-gsap.to(cardFrame, {
-    xPercent: -15 * (panels.length - 1),
-    ease: "none",
-    scrollTrigger: {
-        trigger: cardWrap,
-        pin: cardWrap,
-        // start: "center center",
-        start: "center 50%",
-        // end: "bottom bottom",
-        end: "bottom 10%",
-        pinSpacing: true,
-        scrub: true,
-        snap: {
-            snapTo: 1 / (panels.length - 1),
+mm.add("(min-width: 769px)", () => {
+    gsap.to(cardFrame, {
+        xPercent: -15 * (panels.length - 1),
+        ease: "none",
+        scrollTrigger: {
+            trigger: cardWrap,
+            pin: cardWrap,
+            start: "center 50%",
+            end: "bottom 10%",
+            pinSpacing: true,
+            scrub: true,
+            snap: {
+                snapTo: 1 / (panels.length - 1),
+            },
         },
-    },
+    });
 });
-// });
 
 // 드롭다운 메뉴
 const myDropDownBtn = document.getElementById("footer_dropdown_btn");
